2e1649c10afab78b4e7357abd1b688f7c98bb981
Frank McCoy <frankmccoy.d@gmail.com>
2017-10-24 17:33:36 -0500

    package stubs

---
+++ src/Pantry/Facades/PantryFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pantry\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PantryFacade extends Facade
+{
+  public function pantry()
+  {
+    //
+  }
+}
---
+++ src/Pantry/Providers/PantryServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pantry\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PantryServiceProvider extends ServiceProvider
+{
+  public function boot()
+  {
+    //
+  }
+
+  public function register()
+  {
+    //
+  }
+}
